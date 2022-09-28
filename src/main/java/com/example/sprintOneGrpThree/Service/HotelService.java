0afcbed0eed776b855b3c5d162afc2573a58629d
poojary-shiv27 <111584066+poojary-shiv27@users.noreply.github.com>
2022-09-28 14:56:00 +0530

    Delete src directory

--- src/main/java/com/example/sprintOneGrpThree/Service/HotelService.java
+++
@@ -1,9 +0,0 @@
-package com.example.sprintOneGrpThree.Service;
-
-import com.example.sprintOneGrpThree.Entity.Hotel;
-
-public interface HotelService {
-
-	String addHotel(Hotel h);
-
-}
--- src/main/java/com/example/sprintOneGrpThree/Service/TransactionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.sprintOneGrpThree.Service;
-
-import com.example.sprintOneGrpThree.Entity.Transaction;
-
-public interface TransactionService {
-
-	String addTransaction(Transaction t);
-
-}
